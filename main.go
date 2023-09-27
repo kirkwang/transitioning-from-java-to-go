50bc094d22b70600c6739becc6deca1b93f2acf3
kirkwang <kirkwangke@gmail.com>
2023-09-26 18:14:00 -0700

    slice and array example

--- main.go
+++ main.go
@@ -4,10 +4,16 @@ import "fmt"
 
 func main() {
 	cities := make([]string, 1, 1)
+
 	copy := cities
 	cities[0] = "London"
+
 	cities = append(cities, "New York")
-	
-	fmt.Printf("[%p]cities = %v, len = %v, cap = %v\n", &cities, cities, len(cities), cap(cities))
-	fmt.Printf("[%p]copy = %v, len = %v, cap = %v\n", &copy, copy, len(copy), cap(copy))
+
+	cities[0] = "Toyko"
+	copy = append(copy, "san francisco")
+
+	fmt.Printf("address [%p]cities = %v, len = %v, cap = %v\n", &cities, cities, len(cities), cap(cities))
+
+	fmt.Printf("address [%p]copy = %v, len = %v, cap = %v\n", &copy, copy, len(copy), cap(copy))
 }
